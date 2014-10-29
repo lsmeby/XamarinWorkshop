5e3ca2b27ac3e772d04bb9d3e55a5a79ff8790eb
Nils-Helge Garli Hegvik <nilsga@gmail.com>
2014-10-29 21:19:46 +0100

    Added binding demo

---
+++ Demos/BindingBasics/BindingBasics/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using BindingBasics.Pages;
+
+namespace BindingBasics
+{
+	public class App
+	{
+		public static Page GetMainPage ()
+		{	
+			return new BindingDemoPage ();
+		}
+	}
+}
+
---
+++ Demos/BindingBasics/BindingBasics/Pages/BindingDemoPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace BindingBasics.Pages
+{	
+	public partial class BindingDemoPage : ContentPage
+	{	
+		public BindingDemoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
