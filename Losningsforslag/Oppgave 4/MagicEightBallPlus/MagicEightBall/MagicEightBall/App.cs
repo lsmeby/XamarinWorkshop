debc54aa4ca00437fe6e38e7bb144e28e092c672
Lars Smeby <lars.smeby@bekk.no>
2014-10-31 14:15:04 +0100

    Added GUI for Magig 8 Ball

--- Losningsforslag/Oppgave 4/MagicEightBallPlus/MagicEightBall/MagicEightBall/App.cs
+++ Losningsforslag/Oppgave 4/MagicEightBallPlus/MagicEightBall/MagicEightBall/App.cs
@@ -9,59 +9,9 @@ namespace MagicEightBall
 {
     public class App
     {
-        private static readonly string[] _options =
-        {
-            "It is certain",
-            "It is decidedly so",
-            "Without a doubt",
-            "Yes definitely",
-            "You may rely on it",
-            "As I see it, yes",
-            "Most likely",
-            "Outlook good", 
-            "Yes",
-            "Signs point to yes",
-            "Reply hazy try again",
-            "Ask again later",
-            "Better not tell you now",
-            "Cannot predict now",
-            "Concentrate and ask again",
-            "Don't count on it",
-            "My reply is no",
-            "My sources say no",
-            "Outlook not so good",
-            "Very doubtful"
-        };
-
         public static Page GetMainPage()
         {
-            var label = new Label
-            {
-                Text = "Ask me anything!",
-                Font = Font.SystemFontOfSize(NamedSize.Large),
-                HorizontalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            Xamarin.Forms.DependencyService.Get<IAccelerometer>().Shaked += (sender, e) =>
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                    {
-                        label.Text = _options[new Random().Next(0, _options.Count())];
-                    });
-            };
-
-            return new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    Padding = new Thickness(20),
-                    VerticalOptions = LayoutOptions.Center,
-                    Children =
-                    {
-                        label
-                    }
-                }
-            };
+            return new MainPage();
         }
     }
 }
---
+++ Losningsforslag/Oppgave 4/MagicEightBallPlus/MagicEightBall/MagicEightBall/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MagicEightBall
+{
+    public partial class MainPage
+    {
+        private static readonly string[] _options =
+        {
+            "It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good", 
+            "Yes",
+            "Signs point to yes",
+            "Reply hazy try again",
+            "Ask again later",
+            "Better not tell you now",
+            "Cannot predict now",
+            "Concentrate and ask again",
+            "Don't count on it",
+            "My reply is no",
+            "My sources say no",
+            "Outlook not so good",
+            "Very doubtful"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            EightBallImage.Source = ImageSource.FromResource("MagicEightBall.Images.8-ball.png");
+
+            Xamarin.Forms.DependencyService.Get<IAccelerometer>().Shaked += (sender, e) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Fortune.Text = _options[new Random().Next(0, _options.Count())];
+                });
+            };
+        }
+    }
+}
